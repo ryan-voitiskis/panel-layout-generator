fc9ad1dd36f85aead29cd7f4c3b7281a718f5b8e
ryan-voitiskis <ryanvoitiskis@pm.me>
2023-01-12 22:54:07 +1100

    extract main controls to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,7 @@
 <template>
   <div class="app">
     <div class="controls">
-      <div class="row-col-controls">
-        <label for="number_of_rows">Rows</label>
-        <input
-          type="number"
-          min="1"
-          max="50"
-          id="number_of_rows"
-          v-model="store.numberOfRows"
-        />
-        <label for="number_of_columns">Columns</label>
-        <input
-          type="number"
-          min="1"
-          max="50"
-          id="number_of_columns"
-          v-model="store.numberOfColumns"
-        />
-      </div>
-      <div class="colours hide-on-mobile">
-        <ColourSwatch
-          v-for="(colour, index) in store.panelColours"
-          :colour="colour"
-          :index="index"
-          :key="index"
-        />
-      </div>
-      <div class="button-controls">
-        <button
-          class="square-icon-button hide-on-large"
-          @click="store.showColourControls = true"
-          id="open_edit_colours"
-        >
-          <PaletteIcon /><label>Edit colours</label>
-        </button>
-        <button
-          class="square-icon-button hide-on-mobile"
-          @click="store.showColourPicker = true"
-          id="open_add_colour"
-        >
-          <PaletteIcon /><label>Add colour</label>
-        </button>
-        <button
-          class="square-icon-button hide-on-mobile"
-          @click="store.showNumbers = !store.showNumbers"
-          id="toggle_numbers"
-        >
-          <GridIcon /><label
-            >{{ store.showNumbers ? "Hide" : "Show" }} numbers</label
-          >
-        </button>
-        <button
-          class="square-icon-button large"
-          @click="store.attemptGenerateMatrix"
-          id="shuffle"
-        >
-          <ShuffleIcon />
-          <label>{{ store.matrix.length ? "Shuffle" : "Generate" }}</label>
-        </button>
-        <div class="secondary-button-controls">
-          <button
-            class="secondary-button"
-            @click="store.showAbout = true"
-            id="open_about"
-          >
-            <InfoIcon />
-          </button>
-          <button
-            class="secondary-button hide-on-mobile"
-            @click="print"
-            id="print"
-          >
-            <PrintIcon />
-          </button>
-          <button
-            class="secondary-button hide-on-large"
-            @click="store.showNumbers = !store.showNumbers"
-            id="toggle_numbers_mobile"
-          >
-            <GridIcon />
-          </button>
-        </div>
-      </div>
+      <MainControls />
     </div>
     <p v-if="store.notEnoughPanels" id="not_enough_panels_msg">
       Not enough panels to fill the grid.
@@ -129,17 +48,12 @@
 <script setup lang="ts">
 import { computed, watch } from "vue"
 import { matrixStore } from "./matrixStore"
-import ColourSwatch from "./components/ColourSwatch.vue"
 import ColourPickerModal from "./components/ColourPickerModal.vue"
 import ColourControlsModal from "./components/ColourControlsModal.vue"
 import PanelMatrix from "./components/PanelMatrix.vue"
-import PaletteIcon from "./components/icons/PaletteIcon.vue"
-import ShuffleIcon from "./components/icons/ShuffleIcon.vue"
-import InfoIcon from "./components/icons/InfoIcon.vue"
 import AboutModal from "./components/AboutModal.vue"
 import ModalBox from "./components/ModalBox.vue"
-import PrintIcon from "./components/icons/PrintIcon.vue"
-import GridIcon from "./components/icons/GridIcon.vue"
+import MainControls from "./components/MainControls.vue"
 const store = matrixStore()
 
 const screenHeight = window.innerHeight - 140
@@ -150,12 +64,6 @@ const panelDimension = computed(() =>
   panelHeight.value < panelWidth.value
     ? panelHeight.value + "px"
     : panelWidth.value + "px"
-)
-
-const print = () => window.print()
-
-const coloursTemplateColumns = computed(
-  () => `repeat(${store.panelColours.length}, 120px)`
 )
 
 watch(
@@ -195,142 +103,10 @@ store.attemptGenerateMatrix()
   padding: 0 10px;
 }
 
-.row-col-controls {
-  align-self: center;
-  display: grid;
-  grid-template-rows: 38px 38px;
-  grid-template-columns: 1fr 100px;
-  gap: 10px;
-  label {
-    line-height: 38px;
-    &[for="number_of_rows"] {
-      grid-template-areas: 1 / 1 / 2 / 2;
-    }
-    &[for="number_of_columns"] {
-      grid-template-areas: 1 / 2 / 2 / 3;
-    }
-  }
-  #number_of_rows {
-    grid-template-areas: 2 / 1 / 3 / 2;
-  }
-  #number_of_columns {
-    grid-template-areas: 2 / 2 / 3 / 3;
-  }
-}
-
-.colours {
-  height: 138px; // 140px - 2px so scroll bar doesn't touch grid
-  padding: 10px 0;
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: v-bind(coloursTemplateColumns);
-  overflow-x: scroll;
-  overflow-y: hidden;
-}
-
-.button-controls {
-  display: flex;
-  gap: 10px;
-  height: 140px;
-  padding: 10px 0;
-  .square-icon-button {
-    display: grid;
-    grid-template-rows: 80px 40px;
-    width: 120px;
-    height: 120px;
-    svg {
-      margin: auto;
-      height: 60px;
-      width: 60px;
-    }
-    label {
-      font-size: 16px;
-      vertical-align: middle;
-    }
-  }
-
-  .secondary-button-controls {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    .secondary-button {
-      width: 55px;
-      height: 55px;
-      svg {
-        height: 30px;
-        width: 30px;
-      }
-    }
-  }
-}
-
-@media screen and (max-width: 840px) {
-  .hide-on-mobile {
-    display: none !important;
-  }
-  .row-col-controls {
-    width: 100px;
-    grid-template-columns: 1fr;
-    gap: 0;
-    grid-template-rows: 22px 36px 26px 36px;
-    label {
-      line-height: 22px;
-      &[for="number_of_rows"] {
-        grid-template-areas: 1 / 1 / 2 / 2;
-      }
-      &[for="number_of_columns"] {
-        margin-top: 4px;
-        grid-template-areas: 1 / 3 / 2 / 4;
-      }
-    }
-    input {
-      width: 100%;
-      height: 36px;
-    }
-    #number_of_rows {
-      grid-template-areas: 1 / 2 / 2 / 3;
-    }
-    #number_of_columns {
-      grid-template-areas: 1 / 4 / 2 / 5;
-    }
-  }
-}
-
-@media screen and (min-width: 841px) {
-  .hide-on-large {
-    display: none !important;
-  }
-}
-
-@media screen and (max-width: 448px) {
-  .button-controls {
-    .square-icon-button {
-      width: 100px;
-    }
-  }
-}
-
-@media screen and (max-width: 408px) {
-  .button-controls {
-    .square-icon-button {
-      width: 80px;
-      label {
-        font-size: 14px;
-      }
-    }
-  }
-}
-
 @media screen and (max-width: 368px) {
   .controls {
     gap: 6px;
     padding: 0;
-  }
-  .button-controls {
-    gap: 6px;
-  }
-  .row-col-controls {
-    width: 80px;
   }
 }
 
---
+++ src/components/MainControls.vue
@@ -0,0 +1,247 @@
+<template>
+  <div class="row-col-controls">
+    <label for="number_of_rows">Rows</label>
+    <input
+      type="number"
+      min="1"
+      max="50"
+      id="number_of_rows"
+      v-model="store.numberOfRows"
+    />
+    <label for="number_of_columns">Columns</label>
+    <input
+      type="number"
+      min="1"
+      max="50"
+      id="number_of_columns"
+      v-model="store.numberOfColumns"
+    />
+  </div>
+  <div class="colours hide-on-mobile">
+    <ColourSwatch
+      v-for="(colour, index) in store.panelColours"
+      :colour="colour"
+      :index="index"
+      :key="index"
+    />
+  </div>
+  <div class="button-controls">
+    <button
+      class="square-icon-button hide-on-large"
+      @click="store.showColourControls = true"
+      id="open_edit_colours"
+    >
+      <PaletteIcon /><label>Edit colours</label>
+    </button>
+    <button
+      class="square-icon-button hide-on-mobile"
+      @click="store.showColourPicker = true"
+      id="open_add_colour"
+    >
+      <PaletteIcon /><label>Add colour</label>
+    </button>
+    <button
+      class="square-icon-button hide-on-mobile"
+      @click="store.showNumbers = !store.showNumbers"
+      id="toggle_numbers"
+    >
+      <GridIcon /><label
+        >{{ store.showNumbers ? "Hide" : "Show" }} numbers</label
+      >
+    </button>
+    <button
+      class="square-icon-button large"
+      @click="store.attemptGenerateMatrix"
+      id="shuffle"
+    >
+      <ShuffleIcon />
+      <label>{{ store.matrix.length ? "Shuffle" : "Generate" }}</label>
+    </button>
+    <div class="secondary-button-controls">
+      <button
+        class="secondary-button"
+        @click="store.showAbout = true"
+        id="open_about"
+      >
+        <InfoIcon />
+      </button>
+      <button class="secondary-button hide-on-mobile" @click="print" id="print">
+        <PrintIcon />
+      </button>
+      <button
+        class="secondary-button hide-on-large"
+        @click="store.showNumbers = !store.showNumbers"
+        id="toggle_numbers_mobile"
+      >
+        <GridIcon />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, watch } from "vue"
+import { matrixStore } from "../matrixStore"
+import ColourSwatch from "./ColourSwatch.vue"
+import PaletteIcon from "./icons/PaletteIcon.vue"
+import ShuffleIcon from "./icons/ShuffleIcon.vue"
+import InfoIcon from "./icons/InfoIcon.vue"
+import PrintIcon from "./icons/PrintIcon.vue"
+import GridIcon from "./icons/GridIcon.vue"
+const store = matrixStore()
+
+const print = () => window.print()
+
+const coloursTemplateColumns = computed(
+  () => `repeat(${store.panelColours.length}, 120px)`
+)
+
+watch(
+  () => store.numberOfRows,
+  () => store.attemptGenerateMatrix()
+)
+
+watch(
+  () => store.numberOfColumns,
+  () => store.attemptGenerateMatrix()
+)
+
+store.attemptGenerateMatrix()
+</script>
+
+<style lang="scss" scoped>
+.row-col-controls {
+  align-self: center;
+  display: grid;
+  grid-template-rows: 38px 38px;
+  grid-template-columns: 1fr 100px;
+  gap: 10px;
+  label {
+    line-height: 38px;
+    &[for="number_of_rows"] {
+      grid-template-areas: 1 / 1 / 2 / 2;
+    }
+    &[for="number_of_columns"] {
+      grid-template-areas: 1 / 2 / 2 / 3;
+    }
+  }
+  #number_of_rows {
+    grid-template-areas: 2 / 1 / 3 / 2;
+  }
+  #number_of_columns {
+    grid-template-areas: 2 / 2 / 3 / 3;
+  }
+}
+
+.colours {
+  height: 138px; // 140px - 2px so scroll bar doesn't touch grid
+  padding: 10px 0;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: v-bind(coloursTemplateColumns);
+  overflow-x: scroll;
+  overflow-y: hidden;
+}
+
+.button-controls {
+  display: flex;
+  gap: 10px;
+  height: 140px;
+  padding: 10px 0;
+  .square-icon-button {
+    display: grid;
+    grid-template-rows: 80px 40px;
+    width: 120px;
+    height: 120px;
+    svg {
+      margin: auto;
+      height: 60px;
+      width: 60px;
+    }
+    label {
+      font-size: 16px;
+      vertical-align: middle;
+    }
+  }
+
+  .secondary-button-controls {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    .secondary-button {
+      width: 55px;
+      height: 55px;
+      svg {
+        height: 30px;
+        width: 30px;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 840px) {
+  .hide-on-mobile {
+    display: none !important;
+  }
+  .row-col-controls {
+    width: 100px;
+    grid-template-columns: 1fr;
+    gap: 0;
+    grid-template-rows: 22px 36px 26px 36px;
+    label {
+      line-height: 22px;
+      &[for="number_of_rows"] {
+        grid-template-areas: 1 / 1 / 2 / 2;
+      }
+      &[for="number_of_columns"] {
+        margin-top: 4px;
+        grid-template-areas: 1 / 3 / 2 / 4;
+      }
+    }
+    input {
+      width: 100%;
+      height: 36px;
+    }
+    #number_of_rows {
+      grid-template-areas: 1 / 2 / 2 / 3;
+    }
+    #number_of_columns {
+      grid-template-areas: 1 / 4 / 2 / 5;
+    }
+  }
+}
+
+@media screen and (min-width: 841px) {
+  .hide-on-large {
+    display: none !important;
+  }
+}
+
+@media screen and (max-width: 448px) {
+  .button-controls {
+    .square-icon-button {
+      width: 100px;
+    }
+  }
+}
+
+@media screen and (max-width: 408px) {
+  .button-controls {
+    .square-icon-button {
+      width: 80px;
+      label {
+        font-size: 14px;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 368px) {
+  .button-controls {
+    gap: 6px;
+  }
+  .row-col-controls {
+    width: 80px;
+  }
+}
+</style>
